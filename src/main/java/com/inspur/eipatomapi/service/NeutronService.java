f26ebb1897424df1a8bc7fc60495fed42bc5cffb
hllu <8038370@qq.com>
2018-09-23 16:41:27 +0800

    path error bugfix

--- src/main/java/com/inspur/eipatomapi/service/NeutronService.java
+++ src/main/java/com/inspur/eipatomapi/service/NeutronService.java
@@ -1,5 +1,6 @@
 package com.inspur.eipatomapi.service;
 
+import com.inspur.eipatomapi.util.CommonUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.inspur.eipatomapi.util.CommonUtil;
