9f50c8968d3b573da0b44aedaaef4f2ce047ae9c
jiasirui <jiasirui@inspur.com>
2018-09-26 18:35:21 +0800

    add DB REST interface

--- src/main/java/com/inspur/eipatomapi/repository/EipPoolRepository.java
+++ src/main/java/com/inspur/eipatomapi/repository/EipPoolRepository.java
@@ -2,10 +2,12 @@ package com.inspur.eipatomapi.repository;
 
 import com.inspur.eipatomapi.entity.EipPool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "eippool", path = "eippool")
 public interface EipPoolRepository extends JpaRepository<EipPool,String> {
 
 
