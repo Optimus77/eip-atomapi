49fad845c4cce84e7ca3e8bc5d28bd130718f095
hllu <8038370@qq.com>
2018-11-07 23:35:38 +0800

    add param check

--- src/main/java/com/inspur/eipatomapi/service/impl/EipServiceImpl.java
+++ src/main/java/com/inspur/eipatomapi/service/impl/EipServiceImpl.java
@@ -97,6 +97,7 @@ public class EipServiceImpl implements IEipService {
         EipAllocateParam eipAllocateParam = new EipAllocateParam();
         eipAllocateParam.setPurchasetime(eipOrder.getReturnConsoleMessage().getDuration());
         List<EipOrderProduct> eipOrderProducts = eipOrder.getReturnConsoleMessage().getProductList();
+        eipAllocateParam.setChargetype(eipOrder.getReturnConsoleMessage().getBillType());
         for(EipOrderProduct eipOrderProduct: eipOrderProducts){
             if(!eipOrderProduct.getProductLineCode().equals("EIP")){
                 continue;
