cf34a4cda27bbe96baa53e10dae1643a80221809
桑新靖 <sangxinjing@inspur.com>
2018-11-21 21:12:42 +0800

    修改eip接口url的list参数

--- src/main/java/com/inspur/eipatomapi/controller/EipController.java
+++ src/main/java/com/inspur/eipatomapi/controller/EipController.java
@@ -74,7 +74,7 @@ public class EipController {
     }
 
     @ICPControllerLog
-    @GetMapping(value = "/eips")
+    @GetMapping(value = "/eips/list")
     @CrossOrigin(origins = "*",maxAge = 3000)
     @ApiOperation(value="listeip",notes="list")
     public ResponseEntity listEip(@RequestParam(required = false) String currentPage , @RequestParam(required = false )String limit) {
