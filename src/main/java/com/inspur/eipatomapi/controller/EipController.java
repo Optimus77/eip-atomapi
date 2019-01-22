61299eea03a473c84526f6921364e1944d604988
路海龙 <luhailong@inspur.com>
2019-01-22 17:32:38 +0800

    unbind ecs bugfix

--- src/main/java/com/inspur/eipatomapi/controller/EipController.java
+++ src/main/java/com/inspur/eipatomapi/controller/EipController.java
@@ -164,9 +164,10 @@ public class EipController {
         String msg="";
         EipUpdateParam updateParam = param.getEipUpdateParam();
 
-        if (updateParam.getServerId() != null){
+        if (updateParam.getServerId() != null || updateParam.getPortId()!= null){
             //may be unbind oprate or bind oprate,use this param ,chargetype and bindwidth do nothing
-            if (updateParam.getServerId().trim().equals("")) {
+            if ((updateParam.getServerId() != null && updateParam.getServerId().trim().equals("")) ||
+                    (updateParam.getPortId() != null && updateParam.getPortId().trim().equals(""))) {
                 log.info("unbind operate, eipid:{}, param:{} ", eipId, updateParam);
                 return eipService.unBindPort(eipId);
 
